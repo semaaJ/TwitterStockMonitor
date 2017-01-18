94bb85312cb08e96efbb4ca0b7c1bd8005442e28
Drew McArthur <drewmcarthur1@gmail.com>
2017-01-18 14:02:28 +0000

    added logger function and fs

--- site/app.js
+++ site/app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var fs = require('fs');
 
 // routes
 app.set('views', __dirname + "/public");
@@ -19,3 +20,14 @@ var port = 8000;
 http.listen(port, function() {
 	logger("Server is running on port " + port);
 });
+
+var logger = function(msg) {
+	console.log(msg);
+	fs.appendFile(
+		__dirname + "/server.log", 
+		new Date().toUTCString() + "	" + msg.toString() + "\n", 
+		function(err){ 
+			if(err) { console.log(err); } 
+		}
+	);
+}
